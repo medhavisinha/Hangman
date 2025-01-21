865126b7c7b97ea05e8619542a898542c402e68a
Medhavi Sinha <onemoremedhavi@gmail.com>
2025-01-21 12:15:26 +0530

    Add files via upload

---
+++ hangman/hangman_words.py
@@ -0,0 +1,28 @@
+word_list = [
+    # list of animals
+    "elephant", "kangaroo", "alligator", "butterfly", "cheetah", "dolphin", "giraffe", "hamster", 
+    "iguana", "jaguar", "koala", "lemur", "mongoose", "narwhal", "octopus", "penguin", "quail", 
+    "rhinoceros", "squirrel", "tarantula", "unicorn", "vulture", "walrus", "xenopus", "yak", "zebra",
+    
+    # list of professions
+    "architect", "biologist", "carpenter", "dentist", "engineer", "firefighter", "geologist", 
+    "historian", "illustrator", "journalist", "lawyer", "mathematician", "nurse", "optician", 
+    "pharmacist", "quartermaster", "radiologist", "scientist", "teacher", "umpire", "veterinarian", 
+    "writer", "xylophonist", "yachtsman", "zoologist",
+    
+    # list of objects
+    "backpack", "calculator", "drumstick", "envelope", "flashlight", "guitar", "headphones", 
+    "jigsaw", "kettle", "lampshade", "microscope", "notebook", "oven", "pencil", "quilt", "remote", 
+    "sunglasses", "teapot", "umbrella", "violin", "wallet", "xylophone", "yarn", "zipper",
+    
+    # list of places
+    "airport", "bridge", "castle", "desert", "everest", "forest", "glacier", "harbor", "island", 
+    "jungle", "kilimanjaro", "lighthouse", "mountain", "nile", "ocean", "park", "quarry", "rainforest", 
+    "savanna", "temple", "university", "village", "waterfall", "xanadu", "yosemite", "zoo",
+    
+    # list of miscellaneous words
+    "adventure", "balloon", "chocolate", "diamond", "energy", "friendship", "galaxy", "happiness", 
+    "imagination", "justice", "knowledge", "laughter", "mystery", "novel", "opportunity", 
+    "puzzle", "question", "rainbow", "strategy", "technology", "universe", "victory", "wisdom", 
+    "xenon", "yesterday", "zephyr"
+]
